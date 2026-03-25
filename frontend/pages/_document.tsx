dad45aa68096c7e4e02552d8618bcc55272560ba
yafiiviviana <splightmlight@gmail.com>
2026-03-25 19:54:02 +0700

    Set globe favicon for browser tab

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/favicon-globe.svg" type="image/svg+xml" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
       <body>
